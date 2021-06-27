19b6d7eba547b3b7b18aea8f4398b0a2ec3c1c1f
Kevin Alanis <kevinjosealanis@gmail.com>
2021-06-27 00:06:51 -0600

    log in vista listo, falta unir con el api

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import {Md5} from 'ts-md5/dist/md5';
+
+//import { UserLoginService } from '../services/user-login.service';
+//import { StudentCourseService } from './../Student/student-services/student-course.service';
+//import { StudentService } from './../Student/student-services/student.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class loginComponent {
-  title="XTECTutor";
-  home= "http://localhost:4200/home"
-  router: any;
+export class loginComponent implements OnInit {
+  hidePassword= true;
+
+  userType: number = 1; 
+  LogInForm: FormGroup;
+  error: string;
+
+
+  constructor(private formB: FormBuilder, private router: Router, /*private userLoginService: UserLoginService,private StudentService:StudentService*/) { 
+    this.LogInForm = this.formB.group({
+    userID: [''],
+    password: ['']
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(formData: any){
+  }
 
   singIn(){
-    return true;
+    this.router.navigateByUrl('home');
   }
 }
